a705fa3115a3a5abc3215acc681c789f144ab078
Maryna Khvastova <maryna.khvastova@htw-berlin.de>
2018-08-01 14:54:22 +0200

    Edit screen

--- src/main/resources/META-INF/resources/scripts/xnat-on-fhir/add-row.js
+++ src/main/resources/META-INF/resources/scripts/xnat-on-fhir/add-row.js
@@ -27,5 +27,113 @@ function addRow() {
 
     new_table.id = "identifierTable" + num_fields;
     new_table.style.display = "block";
-    f.appendChild(new_table);
+    table.parentNode.appendChild(new_table);
+}
+
+function addNameRow() {
+    var f = document.forms.patientForm;
+
+    var table = document.getElementById("nameTable");
+    var base = table.dataset.base;
+    var exitsingTables = document.getElementsByClassName("nameTable");
+    num_fields = exitsingTables.length;
+
+    var new_table = table.cloneNode(true);
+    new_table.className = "nameTable";
+
+    var input_elements = new_table.getElementsByTagName("input");
+    var select_elements = new_table.getElementsByTagName("select");
+    for (var i = 0; i < input_elements.length; i++) {
+        var e = input_elements[i];
+        e.id = base + "[" + num_fields + "]/" + e.name;
+        e.name = e.id;
+    }
+
+    for (var i = 0; i < select_elements.length; i++) {
+        var e = select_elements[i];
+        e.id = base + "[" + num_fields + "]/" + e.name;
+        e.name = e.id;
+    }
+
+    new_table.id = "nameTable" + num_fields;
+    new_table.style.display = "block";
+    table.parentNode.appendChild(new_table);
+}
+
+function addContactRow() {
+    var f = document.forms.patientForm;
+
+    var table = document.getElementById("telecomTable");
+    var base = table.dataset.base;
+    var exitsingTables = document.getElementsByClassName("telecomTable");
+    num_fields = exitsingTables.length;
+
+    var new_table = table.cloneNode(true);
+    new_table.className = "telecomTable";
+
+    var input_elements = new_table.getElementsByTagName("input");
+    var select_elements = new_table.getElementsByTagName("select");
+    for (var i = 0; i < input_elements.length; i++) {
+        var e = input_elements[i];
+        e.id = base + "[" + num_fields + "]/" + e.name;
+        e.name = e.id;
+    }
+
+    for (var i = 0; i < select_elements.length; i++) {
+        var e = select_elements[i];
+        e.id = base + "[" + num_fields + "]/" + e.name;
+        e.name = e.id;
+    }
+
+    new_table.id = "telecomTable" + num_fields;
+    new_table.style.display = "block";
+    table.parentNode.appendChild(new_table);
+}
+
+function fix_input_names(base, fields, counter) {
+    for (var i = 0; i < fields.length; i++) {
+        var e = fields[i];
+        e.id = base + "[" + counter + "]/" + e.name;
+        e.name = e.id;
+    }
+}
+
+function addAddressRow() {
+    var f = document.forms.patientForm;
+
+    var table = document.getElementById("addressTable");
+    var base = table.dataset.base;
+    var exitsingTables = document.getElementsByClassName("addressTable");
+    num_fields = exitsingTables.length;
+
+    var new_table = table.cloneNode(true);
+    new_table.className = "addressTable";
+
+    fix_input_names(base, new_table.getElementsByTagName("input"), num_fields);
+    fix_input_names(base, new_table.getElementsByTagName("select"), num_fields);
+    fix_input_names(base, new_table.getElementsByTagName("textarea"), num_fields);
+
+    new_table.id = "addressTable" + num_fields;
+    new_table.style.display = "block";
+    table.parentNode.appendChild(new_table);
+}
+
+function addPhoto() {
+    var f = document.forms.patientForm;
+
+    var table = document.getElementById("photoTable");
+    var base = table.dataset.base;
+    var exitsingTables = document.getElementsByClassName("photoTable");
+    num_fields = exitsingTables.length;
+
+    var new_table = table.cloneNode(true);
+    new_table.className = "photoTable";
+
+    fix_input_names(base, new_table.getElementsByTagName("input"), num_fields);
+    fix_input_names(base, new_table.getElementsByTagName("select"), num_fields);
+    fix_input_names(base, new_table.getElementsByTagName("textarea"), num_fields);
+
+    new_table.id = "photoTable" + num_fields;
+    new_table.style.display = "block";
+    table.parentNode.appendChild(new_table);
 }
--- src/main/resources/META-INF/resources/scripts/xnat-on-fhir/delete-row.js
+++ src/main/resources/META-INF/resources/scripts/xnat-on-fhir/delete-row.js
@@ -6,3 +6,39 @@ function deleteRow(element) {
 
     element.parentNode.removeChild(element);
 }
+
+function deleteNameRow(element) {
+    // Removes an element from the document
+    while (element.className !== "nameTable") {
+        element = element.parentNode;
+    }
+
+    element.parentNode.removeChild(element);
+}
+
+function deleteContactRow(element) {
+    // Removes an element from the document
+    while (element.className !== "telecomTable") {
+        element = element.parentNode;
+    }
+
+    element.parentNode.removeChild(element);
+}
+
+function deleteAddressRow(element) {
+    // Removes an element from the document
+    while (element.className !== "addressTable") {
+        element = element.parentNode;
+    }
+
+    element.parentNode.removeChild(element);
+}
+
+function deletePhoto(element) {
+    // Removes an element from the document
+    while (element.className !== "photoTable") {
+        element = element.parentNode;
+    }
+
+    element.parentNode.removeChild(element);
+}
